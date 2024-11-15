721024d9f84f8979d3e42116164b33cecc662308
Dikshit Bhatta <147680213+DikshitBhatta@users.noreply.github.com>
2024-11-15 22:16:33 +0545

    .

--- urls.py
+++ urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 import views
 
 urlpatterns = [
-    path('', views.header, name='index'),  # Root URL displays header as homepage
+    path('', views.header, name='header'),  # Root URL displays header as homepage
     path('admin-panel/', admin.site.urls),
     path('home/', views.homePage, name='home'),
     path('about/', views.About, name='about'),
