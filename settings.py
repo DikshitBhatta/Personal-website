f4afe17d21db97ea29f7ad6347fb7eecc4fd46ed
Dikshit Bhatta <147680213+DikshitBhatta@users.noreply.github.com>
2024-11-15 21:50:56 +0545

    .

--- settings.py
+++ settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR  / 'templates'],       # Reference `PROJECT_ROOT` here
+        'DIRS': [PROJECT_ROOT / 'templates'],       # Reference `PROJECT_ROOT` here
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,8 +131,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    BASE_DIR  / 'static']
-STATIC_ROOT=os.path.join(BASE_DIR , 'assets')
+    PROJECT_ROOT / 'static']
+STATIC_ROOT=os.path.join(PROJECT_ROOT, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
